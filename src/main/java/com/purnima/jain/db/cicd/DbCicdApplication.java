420f43bb927e5d84dbb88ae631bab3c59baafbf8
Purnima Jain <jain_purnima@hotmail.com>
2025-01-23 20:19:23 +0530

    Initial Commit, Working Code

---
+++ src/main/java/com/purnima/jain/db/cicd/DbCicdApplication.java
@@ -0,0 +1,13 @@
+package com.purnima.jain.db.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/purnima/jain/db/cicd/DbCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purnima.jain.db.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
